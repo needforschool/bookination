2536378e4e6ff595acb73b990f88c7c8cb6269f8
DembaMBOW <demba.pro.76000@gmail.com>
2020-11-11 21:05:30 +0100

    style - home

---
+++ src/template/footer.php
@@ -0,0 +1,9 @@
+    <footer id="footer">
+        <ul>
+            <li><a href="./legal.php">Mentions légales</a></li>
+            <hr>
+            <li>© Tous droits réservés</li>
+        </ul>
+    </footer>
+</body>
+</html>
