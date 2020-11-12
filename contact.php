dea491a5fb56b70f1541197b6d143e2dbf0f57bc
Julienne Sahel <SahelJ>
2020-11-12 17:30:07 +0100

    feat - contact

--- contact.php
+++ contact.php
@@ -1,5 +1,33 @@
 <?php
 
+require('src/inc/pdo.php');
+require('src/inc/functions.php');
+
+$errors = array();
+$sent = false;
+
+if (!empty($_POST['submit'])) {
+
+    $mail = checkXss($_POST['mail']);
+    $firstname = trim(strip_tags($_POST['firstname']));
+    $lastname = trim(strip_tags($_POST['lastname']));
+    $subject = trim(strip_tags($_POST['subject']));
+    $message = trim(strip_tags($_POST['message']));
+
+    checkEmail($errors, $mail, 'mail');
+    checkField($errors, $mail, 'mail', 6, 160);
+    checkField($errors, $firstname, 'firstname', 2, 100);
+    checkField($errors, $lastname, 'lastname', 2, 100);
+    checkField($errors, $subject, 'subject', 10, 250);
+    checkField($errors, $message, 'message', 10, 2000);
+
+    if (count($errors) == 0) {
+        insert($pdo, 'bn_contact', ['mail', 'firstname',  'lastname',  'subject', 'message', 'created_at'], [$mail, $firstname, $lastname, $subject, $message, now()]);
+        $sent = true;
+    }
+}
+
+$title = 'Contact - Bookination';
 include('src/template/header.php');
 ?>
 <section id="contact">
@@ -8,26 +36,24 @@ include('src/template/header.php');
             <form action="" method="POST">
 
                 <div class="inputs-container">
-                    <!-- mail -->
-                    <input type="email" name="mail" placeholder="Votre email"></input>
-                    <span class="error"></span>
-                    <!-- prénom -->
-                    <input type="text" name="firstname" placeholder="Votre prénom"></input>
-                    <span class="error"></span>
+                    <input type="mail" name="mail" placeholder="Votre mail" value="<?= (!empty($_POST['mail'])) ? $_POST['mail'] : '' ?>">
+                    <span class="error"><?= (!empty($errors['mail'])) ? $errors['mail'] : '' ?></span>
+                    <input type="text" name="firstname" placeholder="Votre prénom" value="<?= (!empty($_POST['firstname'])) ? $_POST['firstname'] : '' ?>">
+                    <span class="error"><?= (!empty($errors['firstname'])) ? $errors['firstname'] : '' ?></span>
                 </div>
                 <div class="inputs-container">
-                    <!-- nom -->
-                    <input type="text" name="lastname" placeholder="Votre nom"></input>
-                    <span class="error"></span>
-                    <!-- sujet -->
-                    <input type="text" name="sujbect" placeholder="Votre motif">
-                    <span class="error"></span>
+                    <input type="text" name="lastname" placeholder="Votre nom" value="<?= (!empty($_POST['lastname'])) ? $_POST['lastname'] : '' ?>">
+                    <span class="error"><?= (!empty($errors['lastname'])) ? $errors['lastname'] : '' ?></span>
+                    <input type="text" name="subject" placeholder="Votre motif" value="<?= (!empty($_POST['subject'])) ? $_POST['subject'] : '' ?>">
+                    <span class="error"><?= (!empty($errors['subject'])) ? $errors['subject'] : '' ?></span>
                 </div>
-                <!-- message -->
-                <textarea name="message" placeholder="Votre message"></textarea>
-                <span class="error"></span>
-                <!-- submit -->
-                <input type="submit" name="submit" class="btn btn-purple" value="Envoyer">
+                <textarea name="message" placeholder="Votre message"><?= (!empty($_POST['message'])) ? $_POST['message'] : '' ?></textarea>
+                <span class="error"><?= (!empty($errors['message'])) ? $errors['message'] : '' ?></span>
+                <?php if ($sent == false) : ?>
+                    <input type="submit" name="submit" class="btn btn-purple" value="Envoyer">
+                <?php else : ?>
+                    <input type="submit" class="btn btn-success" value="Bien reçu !" disabled>
+                <?php endif; ?>
             </form>
         </div>
         <div class="contact-image">
@@ -36,4 +62,4 @@ include('src/template/header.php');
     </div>
 </section>
 
-<?php include('src/template/footer.php') ?>
+<?php include('src/template/footer.php');
