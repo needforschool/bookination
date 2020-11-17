2df72addc7a58450c5f8a360039e6ad7501c2755
DembaMBOW <demba.pro.76000@gmail.com>
2020-11-17 13:27:24 +0100

    feat - error
    Co-authored-by: Aurore Fournier <18518@csmrouen.net>

--- error.php
+++ error.php
@@ -2,24 +2,33 @@
 require('src/inc/pdo.php');
 require('src/inc/functions.php');
 
-$error = '404';
-$title = 'Erreur '.$error.' - Bookination';
-
-
+$error = 404;
+if (!empty($_GET['e']) && is_numeric($_GET['e']) && $_GET['e'] >= 400 && $_GET['e'] <= 527) $error = $_GET['e'];
+$message;
+switch ($error) {
+    case 403:
+        $message = 'Accès refusé';
+        break;
+    case 400:
+        $message = 'Syntaxe erronnée';
+        break;
+    default:
+        $message = 'Ressource non trouvée';
+        break;
+}
+$title = ' Erreur ' . $error . ' - Bookination';
 include('src/template/header.php');
 ?>
-
 <section id="error">
     <div class="wrap-fluid">
         <div class="error-text">
-            <h1>Erreur 403</h1>
-            <p>Accès interdit !</p>
+            <h1>Erreur <?= $error ?></h1>
+            <p><?= $message ?></p>
         </div>
         <div class="error-image">
             <img src="assets/img/errors.gif" alt="erreur">
         </div>
     </div>
 </section>
-
-
-<?php include('src/template/footer.php');
+<?php
+include('src/template/footer.php');
