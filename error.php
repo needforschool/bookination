fa71a86a0a4b77686116796064f0b948f13bb5bd
AuroreFournier <aurore.fournier0809@gmail.com>
2020-11-17 10:49:03 +0100

    style - error

---
+++ error.php
@@ -0,0 +1,24 @@
+<?php
+require('src/inc/pdo.php');
+require('src/inc/functions.php');
+
+$title = 'errors - Page not found';
+
+
+include('src/template/header.php');
+?>
+
+<section id="error">
+    <div class="wrap-fluid">
+        <div class="error-text">
+            <h1>Erreur 403</h1>
+            <p>Accès interdit !</p>
+        </div>
+        <div class="error-image">
+            <img src="assets/img/errors.gif" alt="erreur">
+        </div>
+    </div>
+</section>
+
+
+<?php include('src/template/footer.php');
