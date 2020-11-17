1110a9d4b99960738df2eca4eb8124f8383befb6
DembaMBOW <demba.pro.76000@gmail.com>
2020-11-17 16:41:53 +0100

    feat - forgot password
    Co-authored-by: Aurore Fournier <18518@csmrouen.net>

--- forgot_password.php
+++ forgot_password.php
@@ -2,9 +2,27 @@
 require('src/inc/pdo.php');
 require('src/inc/functions.php');
 
+$errors = [];
+$sent = false;
+
+if (!empty($_POST['submit'])) {
+    $mail  = checkXss($_POST['mail']);
+
+    $errors = checkEmail($errors, $mail, 'mail');
+    $checkUsedEmail = select($pdo, 'bn_users', 'mail', 'mail', $mail);
+    if (empty($checkUsedEmail)) $errors['mail'] = 'Email introuvable';
+    else {
+        if (count($errors) == 0) {
+            //TODO: Envoie du mail
+            $sent = true;
+        }
+    }
+}
+
 $title = 'Récupération de mot de passe- Bookination';
 include('src/template/header.php');
 ?>
+
 <section id="forgot_password">
     <div class="wrap-fluid">
         <div class="forgot-form" id="forgot-form">
@@ -12,10 +30,16 @@ include('src/template/header.php');
                 <input type="email" name="mail" placeholder="Votre email" value="<?php if (!empty($_POST['mail'])) echo $_POST['mail'];
                                                                                     ?>">
                 <span class="error"><?= (!empty($errors['mail'])) ? $errors['mail'] : '' ?></span>
-                <input type="submit" name="submit" class="btn btn-purple" value="Envoyer">
+                <?php if ($sent == false) : ?>
+                    <input type="submit" name="submit" class="btn btn-purple" value="Envoyer">
+                <?php else : ?>
+                    <input type="submit" class="btn btn-success" value="Bien reçu !" disabled>
+                <?php endif; ?>
             </form>
         </div>
         <div class="forgot-image">
             <img src="assets\img\undraw_forgot_password_gi2d.svg" alt="Image forgot">
         </div>
 </section>
+
+<?php include('src/template/footer.php');
