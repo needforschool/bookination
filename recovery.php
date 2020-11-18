7606c148b49bb591351da69335cf52bbbcb270d2
Chocolate <etorefr@gmail.com>
2020-11-18 17:03:39 +0100

    fix - recovery missing footer

--- recovery.php
+++ recovery.php
@@ -2,13 +2,14 @@
 require('src/inc/pdo.php');
 require('src/inc/functions.php');
 
+$errors = [];
+
 if (!empty($_GET['mail']) && !empty($_GET['token']) && filter_var($_GET['mail'], FILTER_VALIDATE_EMAIL)) {
     $mail = checkXss($_GET['mail']);
     $token = checkXss($_GET['token']);
 
     $user = select($pdo, 'bn_users', '*', 'mail', $mail);
     if (!empty($user) && $user['token'] == $token) {
-        $errors = [];
         if (!empty($_POST['submit'])) {
             $password = checkXss($_POST['password']);
             $passwordConfirm = checkXss($_POST['password-confirm']);
@@ -57,3 +58,5 @@ include('src/template/header.php');
             <img src="assets\img\undraw_secure_login_pdn4.svg" alt="Image recovery">
         </div>
 </section>
+
+<?php include('src/template/footer.php');
