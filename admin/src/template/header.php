f8d65c449706389c5ff6b0343e7ad25aefee3bd7
Chocolate <etorefr@gmail.com>
2020-11-18 22:15:35 +0100

    feat - admin home

--- admin/src/template/header.php
+++ admin/src/template/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>BackOffice | Dashboard</title>
+    <title>Admin - Bookination</title>
 
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
@@ -38,10 +38,10 @@ to get the desired effect
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="../index.php" class="nav-link">Home</a>
+                    <a href="./../" class="nav-link">Home</a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="../contact.php" class="nav-link">Contact</a>
+                    <a href="./../contact.php" class="nav-link">Contact</a>
                 </li>
             </ul>
         </nav>
@@ -50,9 +50,9 @@ to get the desired effect
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="./" class="brand-link">
                 <img src="../assets/img/logo/logo-square-bg-none.png" alt="Bookination Logo" class="brand-image img-circle elevation-3" style="opacity: 1">
-                <span class="brand-text font-weight-light">Admin / Bookination</span>
+                <span class="brand-text font-weight-light">Admin | Bookination</span>
             </a>
 
             <!-- Sidebar -->
