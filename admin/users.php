cc49dd9b0a6776ce17cf541601f6483fd982f294
Chocolate <etorefr@gmail.com>
2020-11-19 02:40:44 +0100

    fix - manage order

--- admin/users.php
+++ admin/users.php
@@ -9,7 +9,7 @@ if (!isAdmin()) {
     die();
 }
 
-$users=selectAll($pdo, 'bn_users');
+$users = selectAll($pdo, 'bn_users', '*', null, null, 'created_at', 'DESC');
 
 if (!empty($_GET['delete']) && is_numeric($_GET['delete']) && select($pdo, 'bn_users', '*', 'id', $_GET['delete'])) delete($pdo, 'bn_users', 'id', $_GET['delete']);
 
@@ -26,110 +26,110 @@ include('src/template/header.php'); ?>
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
-    
-     <!-- Main content -->
-     <section class="content">
 
-<!-- Default box -->
-<div class="card">
-  <div class="card-header">
-    <h3 class="card-title">Liste complete</h3>
+    <!-- Main content -->
+    <section class="content">
 
-    <div class="card-tools">
-      <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-        <i class="fas fa-minus"></i></button>
-    </div>
-  </div>
-  <div class="card-body p-0">
-    <table class="table table-striped projects">
-        <thead>
-            <tr>
-                <th style="width: 2%">
-                    #
-                </th>
-                <th style="width: 15%">
-                    e-mail
-                </th>
-                <th style="width: 10%">
-                    Firstname
-                </th>
-                <th style="width: 10%">
-                    Lastname
-                </th>
-                <th style="width: 10%">
-                     Date de naissance
-                </th>
-                <th style="width: 10%">
-                    Genre
-                </th>
-                <th style="width: 10%">
-                    Date de création
-                </th>
-                <th style="width: 10%">
-                    Date de màj
-                </th>
-                <th style="width: 8%" class="text-center">
-                    Rôle
-                </th>
-                <th style="width: 15%">
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Liste complete</h3>
 
-                </th>
-                
-            </tr>
-        </thead>
-        <tbody>
-        <?php foreach($users as $user):?>
-            <tr>
-                <td>
-                    <?= $user['id'] ?>
-                </td>
-                <td>
-                    <?= $user['mail'] ?>
-                </td>
-                <td>
-                    <?= $user['firstname'] ?>
-                </td>
-                <td>
-                    <?= $user['lastname'] ?>  
-                </td>
-                <td>
-                    <?= $user['birthdate'] ?>
-                </td>
-                <td>
-                    <?= $user['gender'] ?>
-                </td>
-                <td>
-                    <?= $user['created_at'] ?>
-                </td>
-                <td>
-                    <?= $user['updated_at'] ?>
-                </td>
-                <td class="project-state">
-                    <?= $user['role'] ?>
-                </td>
-                <td class="project-actions text-right">
-                    <a class="btn btn-info btn-sm" href="users_edit.php">
-                        <i class="fas fa-pencil-alt">
-                        </i>
-                        Editer
-                    </a>
-                    <a class="btn btn-danger btn-sm" href="?delete=<?= $user['id'] ?>">
-                        <i class="fas fa-trash">
-                        </i>
-                        Supprimer
-                    </a>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
-  </div>
-  <!-- /.card-body -->
-</div>
-<!-- /.card -->
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                        <i class="fas fa-minus"></i></button>
+                </div>
+            </div>
+            <div class="card-body p-0">
+                <table class="table table-striped projects">
+                    <thead>
+                        <tr>
+                            <th style="width: 2%">
+                                #
+                            </th>
+                            <th style="width: 15%">
+                                e-mail
+                            </th>
+                            <th style="width: 10%">
+                                Firstname
+                            </th>
+                            <th style="width: 10%">
+                                Lastname
+                            </th>
+                            <th style="width: 10%">
+                                Date de naissance
+                            </th>
+                            <th style="width: 10%">
+                                Genre
+                            </th>
+                            <th style="width: 10%">
+                                Date de création
+                            </th>
+                            <th style="width: 10%">
+                                Date de màj
+                            </th>
+                            <th style="width: 8%" class="text-center">
+                                Rôle
+                            </th>
+                            <th style="width: 15%">
 
-</section>
-<!-- /.content -->
+                            </th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($users as $user) : ?>
+                            <tr>
+                                <td>
+                                    <?= $user['id'] ?>
+                                </td>
+                                <td>
+                                    <?= $user['mail'] ?>
+                                </td>
+                                <td>
+                                    <?= $user['firstname'] ?>
+                                </td>
+                                <td>
+                                    <?= $user['lastname'] ?>
+                                </td>
+                                <td>
+                                    <?= $user['birthdate'] ?>
+                                </td>
+                                <td>
+                                    <?= $user['gender'] ?>
+                                </td>
+                                <td>
+                                    <?= $user['created_at'] ?>
+                                </td>
+                                <td>
+                                    <?= $user['updated_at'] ?>
+                                </td>
+                                <td class="project-state">
+                                    <?= $user['role'] ?>
+                                </td>
+                                <td class="project-actions text-right">
+                                    <a class="btn btn-info btn-sm" href="users_edit.php">
+                                        <i class="fas fa-pencil-alt">
+                                        </i>
+                                        Editer
+                                    </a>
+                                    <a class="btn btn-danger btn-sm" href="?delete=<?= $user['id'] ?>">
+                                        <i class="fas fa-trash">
+                                        </i>
+                                        Supprimer
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
 
